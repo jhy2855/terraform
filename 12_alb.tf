e1e0ce0cdd14b4ff16b17c4bbcfc543b4e74ca5d
jhy <yon3685@naver.com>
2021-11-01 09:25:25 +0900

    alb_step2

--- 12_alb.tf
+++ 12_alb.tf
@@ -4,7 +4,7 @@ resource "aws_lb" "jhy_alb" {
   internal              = false
   load_balancer_type    = "application"
   security_groups       = [aws_security_group.jhy_websg.id]
-  subnets               = [aws_subnet.jhy-puba.id,aws_subnet.jhy-pubc.id]
+  subnets               = [aws_subnet.jhy_puba.id,aws_subnet.jhy_pubc.id]
   
   tags = {
     Name  =  "jhy-alb"
