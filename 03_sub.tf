ce5caa4fae97ca9ac7532a593977aa76e088615e
jhy <yon3685@naver.com>
2021-11-01 09:23:17 +0900

    sub_step2

--- 03_sub.tf
+++ 03_sub.tf
@@ -1,5 +1,5 @@
 # 가용영역 a의 Public Subnet
-resource "aws_subnet" "jhy-puba" {
+resource "aws_subnet" "jhy_puba" {
   vpc_id            = aws_vpc.jhy_vpc.id
   cidr_block        = "10.0.0.0/24"
   availability_zone = "ap-northeast-2a"
@@ -10,7 +10,7 @@ resource "aws_subnet" "jhy-puba" {
 }
 
 # 가용영역 a의 Private Subnet
-resource "aws_subnet" "jhy-pria" {
+resource "aws_subnet" "jhy_pria" {
   vpc_id            = aws_vpc.jhy_vpc.id
   cidr_block        = "10.0.1.0/24"
   availability_zone = "ap-northeast-2a"
@@ -21,7 +21,7 @@ resource "aws_subnet" "jhy-pria" {
 }
 
 # 가용영역 c의 Public Subnet
-resource "aws_subnet" "jhy-pubc" {
+resource "aws_subnet" "jhy_pubc" {
   vpc_id            = aws_vpc.jhy_vpc.id
   cidr_block        = "10.0.2.0/24"
   availability_zone = "ap-northeast-2c"
@@ -32,12 +32,31 @@ resource "aws_subnet" "jhy-pubc" {
 }
 
 # 가용영역 c의 Private Subnet
-resource "aws_subnet" "jhy-pric" {
+resource "aws_subnet" "jhy_pric" {
   vpc_id            = aws_vpc.jhy_vpc.id
   cidr_block        = "10.0.3.0/24"
   availability_zone = "ap-northeast-2c"
 
   tags = {
     Name = "jhy-pric"
+  }
+}
+
+  resource "aws_subnet" "jhy_pridba" {
+  vpc_id            = aws_vpc.jhy_vpc.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "ap-northeast-2a"
+
+  tags = {
+    Name = "jhy-pridba"
+  }
+}
+resource "aws_subnet" "jhy_pridbc" {
+  vpc_id            = aws_vpc.jhy_vpc.id
+  cidr_block        = "10.0.5.0/24"
+  availability_zone = "ap-northeast-2c"
+
+  tags = {
+    Name = "jhy-pridbc"
   }
 }
