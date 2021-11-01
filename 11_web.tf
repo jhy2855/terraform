892366e45aad7135a0c62fdb5f169f42563d1f20
jhy <yon3685@naver.com>
2021-11-01 09:24:51 +0900

    web_step2

--- 11_web.tf
+++ 11_web.tf
@@ -3,7 +3,7 @@ data "aws_ami" "amzn" {
 
   filter {
     name = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
 
   filter {
@@ -16,13 +16,13 @@ data "aws_ami" "amzn" {
 }
 
 resource "aws_instance" "jhy_weba" {
-  ami                    = data.aws_ami.amzn.id
+  ami                    = "ami-0e4a9ad2eb120e054"
   instance_type          = "t2.micro"
   key_name               = "tf-key"
   vpc_security_group_ids = [aws_security_group.jhy_websg.id]
   availability_zone      = "ap-northeast-2a"
   private_ip             = "10.0.0.11"
-  subnet_id              = aws_subnet.jhy-puba.id
+  subnet_id              = aws_subnet.jhy_puba.id
   user_data              = file("./install_seoul.sh")
 
   tags = {
