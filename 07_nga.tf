2722389cd15d6f21033dd942ce6ef2019c926758
jhy <yon3685@naver.com>
2021-11-01 09:24:09 +0900

    nga_step2

--- 07_nga.tf
+++ 07_nga.tf
@@ -10,7 +10,7 @@ resource "aws_eip" "lb_ip_c" {
 
 resource "aws_nat_gateway" "jhy_nga_a" {
   allocation_id = aws_eip.lb_ip_a.id
-  subnet_id     = aws_subnet.jhy-pria.id
+  subnet_id     = aws_subnet.jhy_pria.id
   tags = {
     Name = "jhy-nga-a"
   }
@@ -18,7 +18,7 @@ resource "aws_nat_gateway" "jhy_nga_a" {
 
 resource "aws_nat_gateway" "jhy_nga_c" {
   allocation_id = aws_eip.lb_ip_c.id
-  subnet_id     = aws_subnet.jhy-pric.id
+  subnet_id     = aws_subnet.jhy_pric.id
   tags = {
     Name = "jhy-nga-c"
   }
