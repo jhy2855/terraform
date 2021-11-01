f95553f4ad0f4d1df2d792c5c22619c7153af1f1
jhy <yon3685@naver.com>
2021-11-01 09:23:43 +0900

    rtas_step2

--- 06_rtas.tf
+++ 06_rtas.tf
@@ -1,10 +1,10 @@
 
 resource "aws_route_table_association" "jhy_rtas_a" {
-  subnet_id = aws_subnet.jhy-puba.id
+  subnet_id = aws_subnet.jhy_puba.id
   route_table_id = aws_route_table.jhy_rt.id
 }
 
 resource "aws_route_table_association" "jhy_rtas_c" {
-  subnet_id = aws_subnet.jhy-pubc.id
+  subnet_id = aws_subnet.jhy_pubc.id
   route_table_id = aws_route_table.jhy_rt.id
 }
